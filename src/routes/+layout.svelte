f96581a3fadff001a144c578e250d8e2e3613e52
gruseb <gruber.sebastian@johak.at>
2026-02-17 16:29:45 +0100

    localStorage

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,21 @@
 <script>
 	import '../app.css';
 	import favicon from '$lib/assets/favicon.svg';
+	import { store } from '$lib/store.svelte';
 
 	let { children } = $props();
 	let theme = $state('halloween');
+
+	function saveTheme() {
+		localStorage.setItem('theme', theme);
+	}
+
+	$effect(() => {
+	if (localStorage.getItem('theme')) {
+		theme = localStorage.getItem('theme');
+		
+	}
+	});
 </script>
 
 <svelte:head>
@@ -15,11 +27,11 @@
 		<a href="/" class="text-4xl">🐰</a>
 		<select class="select w-[150px]" bind:value={theme}>
 		<option disabled selected>Pick a theme</option>
-		<option value="light">light</option>
-		<option value="dark">dark</option>
-		<option value="cyberpunk">cyberpunk</option>
-		<option value="valentine">valentine</option>
-		<option value="halloween">halloween</option>
+		<option value="light" onclick={saveTheme}>light</option>
+		<option value="dark" onclick={saveTheme}>dark</option>
+		<option value="cyberpunk" onclick={saveTheme}>cyberpunk</option>
+		<option value="valentine" onclick={saveTheme}>valentine</option>
+		<option value="halloween" onclick={saveTheme}>halloween</option>
 	</select>
 </nav>
 <main class="flex flex-col items-center justify-center">
