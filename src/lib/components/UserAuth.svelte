81ef296d0d941a7f9ec154781538b5c64af1ca42
gruseb <gruber.sebastian@johak.at>
2026-02-25 12:19:25 +0100

    Authentication

---
+++ src/lib/components/UserAuth.svelte
@@ -0,0 +1,51 @@
+<script>
+    //Video: 3:40 letztes Video
+	let email = $state('');
+	let username = $state('');
+	let password1 = $state('');
+	let password2 = $state('');
+
+    let registration = $state(false);
+
+	import { pb } from '$lib/store.svelte';
+	async function registerUser() {
+		const data = {
+			email: email,
+            emailVisibility: true,
+			username: username,
+			password: password1,
+			passwordConfirm: password2
+		};
+		const record = await pb.collection('users').create(data);
+	}
+</script>
+
+<div class="card w-96 max-w-96 bg-base-100 shadow-sm">
+	<div class="card-body">
+		<form action="">
+			<h2 class="card-title">{registration ? 'Register' : 'Login'}</h2>
+
+			<fieldset class="fieldset">
+				<legend class="fieldset-legend">Mail</legend>
+				<input type="text" class="input" placeholder="Where can we reach you?" bind:value={email} />
+			</fieldset>
+			<fieldset class="fieldset">
+				<legend class="fieldset-legend">Username</legend>
+				<input
+					type="text"
+					class="input"
+					placeholder="How should we call you?"
+					bind:value={username}
+				/>
+			</fieldset>
+			<fieldset class="fieldset">
+				<legend class="fieldset-legend">Password</legend>
+				<input type="password" class="input" placeholder="password" bind:value={password1} />
+				<input type="password" class="input" placeholder="Please repeat!" bind:value={password2} />
+			</fieldset>
+			<div class="card-actions justify-end">
+				<button class="btn btn-primary" onclick={registerUser}>Register</button>
+			</div>
+		</form>
+	</div>
+</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -11,10 +11,9 @@
 	}
 
 	$effect(() => {
-	if (localStorage.getItem('theme')) {
-		theme = localStorage.getItem('theme');
-		
-	}
+		if (localStorage.getItem('theme')) {
+			theme = localStorage.getItem('theme');
+		}
 	});
 </script>
 
@@ -23,19 +22,18 @@
 </svelte:head>
 
 <div data-theme={theme} class=" min-h-screen p-2">
-	<nav class="flex justify-between items-center">
+	<nav class="flex items-center justify-between">
 		<a href="/" class="text-4xl">🐰</a>
 		<select class="select w-[150px]" bind:value={theme}>
-		<option disabled selected>Pick a theme</option>
-		<option value="light" onclick={saveTheme}>light</option>
-		<option value="dark" onclick={saveTheme}>dark</option>
-		<option value="cyberpunk" onclick={saveTheme}>cyberpunk</option>
-		<option value="valentine" onclick={saveTheme}>valentine</option>
-		<option value="halloween" onclick={saveTheme}>halloween</option>
-	</select>
-</nav>
-<main class="flex flex-col items-center justify-center">
-	{@render children?.()}
-</main>
-	
+			<option disabled selected>Pick a theme</option>
+			<option value="light" onclick={saveTheme}>light</option>
+			<option value="dark" onclick={saveTheme}>dark</option>
+			<option value="cyberpunk" onclick={saveTheme}>cyberpunk</option>
+			<option value="valentine" onclick={saveTheme}>valentine</option>
+			<option value="halloween" onclick={saveTheme}>halloween</option>
+		</select>
+	</nav>
+	<main class="flex min-h-screen flex-col items-center justify-center">
+		{@render children?.()}
+	</main>
 </div>
